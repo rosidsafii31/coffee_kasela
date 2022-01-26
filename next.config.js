0a911bd077d5d0e08f2fce668bad9f6e9d45386e
MohNurSafii <68380642+rosidsafii31@users.noreply.github.com>
2022-01-26 22:28:07 +0700

    baru

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:3000",
+        "BASE_URL": "https://coffee-kasela.vercel.app/",
         "MONGODB_URL": "mongodb+srv://kasela:kasela@kasela.kyqcg.mongodb.net/kasela?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": "jbr]PeEX';Q;,&-Kv(5ryEPaDwaFN'd6nE28_t9W_P^mdk.UzR",
         "REFRESH_TOKEN_SECRET": "9Gj}~Pc.e5V6+7f=c<L72RCd`2~6@'$J$-Ea*\B(=,?JrH4n{(T!C&7sbfAVns\:(#)N7X2NECwg-Qbv4&]SN2FJ",
